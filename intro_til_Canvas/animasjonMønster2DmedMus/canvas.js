718640a0ccfb39d6e8a2181a85b3a05afa81e2f4
mareis <mads.reistadbakk@mrfylke.no>
2018-01-26 12:49:21 +0100

    .

--- intro_til_Canvas/animasjonMønster2DmedMus/canvas.js
+++ intro_til_Canvas/animasjonMønster2DmedMus/canvas.js
@@ -5,35 +5,49 @@ canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 
 var fart = 0.05;
-var temp = 100;
+var temp = 0;
 var musX = canvas.width/2;
 var musY = canvas.height/2;
+var red = 255;
+var green = 0;
+var blue = 100;
+
 
 c.canvas.addEventListener('mousemove', function (e) {
     musX = e.clientX;
     musY = e.clientY;
+});
+c.canvas.addEventListener('click', function () {
+   red = Math.round(Math.random()*255);
+   green = Math.round(Math.random()*255);
+   blue = Math.round(Math.random()*155)+100;
 });
 
 
 function animate() {
     requestAnimationFrame(animate);
 
-    c.fillStyle = 'rgba(0, 0, 0, 0.01)';
+    c.fillStyle = 'rgba(0, 0, 0, 0.06)';
     c.fillRect(0, 0, canvas.width, canvas.height);
+
+    c.font = "30px Arial";
+    c.fillStyle = 'rgba(255,255,255,0.5)';
+    c.fillText("Informasjonsteknologi 1", musX-150, musY);
+
 
     temp = temp + fart;
 
-    for (var i = 0; i < 20; i++) {
-        var x = Math.sin(temp+i*temp/25)* 10*i + musX;
-        var y = Math.cos(temp+i*temp/25)* 10*i + musY;
+    for (var i = 0; i < 40; i++) {
+        var x = Math.cos(Math.log(temp)+i*temp/25)* 10*i + musX;
+        var y = Math.sin(Math.log10(temp)+i*temp/25)* 10*i + musY;
         var radius = 3;
-        var r = 255-Math.round(Math.random()*i*20);
-        var g = 0
-        var b = Math.round(Math.random()*i*10)+100;
+        var r = red-Math.round(Math.random()*i*10);
+        var g = 0;
+        var b = blue-Math.round(Math.random()*i*20);
         c.beginPath();
         c.arc(x, y, radius, 0, 2 * Math.PI);
         c.strokeStyle = 'rgba(' + r + ',' + g + ',' + b + ',1)';
-        c.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',0.4)';
+        c.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',0.8)';
         c.fill();
         c.stroke();
     }
