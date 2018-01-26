9e1608bd91b6f05529d0c831386dc022d009aba1
mareis <mads.reistadbakk@mrfylke.no>
2018-01-26 11:09:52 +0100

    .

--- intro_til_Canvas/animasjonMønster2DmedMus/canvas.js
+++ intro_til_Canvas/animasjonMønster2DmedMus/canvas.js
@@ -6,10 +6,16 @@ var c = canvas.getContext('2d');
 
 var fart = 0.05;
 var temp = 100;
+var musX = canvas.width/2;
+var musY = canvas.height/2;
+
+c.canvas.addEventListener('mousemove', function (e) {
+    musX = e.clientX;
+    musY = e.clientY;
+});
 
 
 function animate() {
-
     requestAnimationFrame(animate);
 
     c.fillStyle = 'rgba(0, 0, 0, 0.01)';
@@ -18,9 +24,9 @@ function animate() {
     temp = temp + fart;
 
     for (var i = 0; i < 20; i++) {
-        var x = Math.sin(temp+i*temp/25)* 20*i + canvas.width/2;
-        var y = Math.cos(temp+i*temp/25)* 20*i + canvas.height/2;
-        var radius = 5;
+        var x = Math.sin(temp+i*temp/25)* 10*i + musX;
+        var y = Math.cos(temp+i*temp/25)* 10*i + musY;
+        var radius = 3;
         var r = 255-Math.round(Math.random()*i*20);
         var g = 0
         var b = Math.round(Math.random()*i*10)+100;
@@ -32,5 +38,4 @@ function animate() {
         c.stroke();
     }
 }
-
 animate();
